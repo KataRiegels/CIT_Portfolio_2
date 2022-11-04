da75f366b671d624e3885ae7d692ed226f24455e
KataRiegels <62016188+KataRiegels@users.noreply.github.com>
2022-11-04 10:35:39 +0100

    Primary project structures

---
+++ Portfolio2_sln/DataLayer/ImdbContext.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using DataLayer.Models;
+
+namespace DataLayer
+{
+    public class ImdbContext : DbContext
+    {
+        const string ConnectionString = "host=localhost;db=imdb;uid=postgres;pwd=Jse33pjp";
+        public DbSet<Title_basics> Title_basicss { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql(ConnectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var tb_mb = modelBuilder.Entity<Title_basics>();
+
+            tb_mb.ToTable("title_basics");
+            tb_mb.Property(x => x.Tconst).HasColumnName("tconst");
+            tb_mb.Property(x => x.TitleTypes).HasColumnName("titletypes");
+            tb_mb.Property(x => x.PrimaryTitle).HasColumnName("primarytitle");
+            tb_mb.Property(x => x.OriginalTitle).HasColumnName("originaltitle");
+            tb_mb.Property(x => x.IsAdult).HasColumnName("isadult");
+            tb_mb.Property(x => x.StartYear).HasColumnName("startyear");
+            tb_mb.Property(x => x.EndYear).HasColumnName("endyear");
+            tb_mb.Property(x => x.RunTimeMinutes).HasColumnName("runtimeminutes");
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Portfolio2_sln/DataLayer/Models/Title_basics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Title_basics
+    {
+
+        public string Tconst { get; set; }
+        public string TitleTypes { get; set; }
+        public string PrimaryTitle { get; set; }
+        public string OriginalTitle { get; set; }
+        public bool IsAdult { get; set; }
+        public int StartYear { get; set; }
+        public int EndYear { get; set; }
+        public int RunTimeMinutes { get; set; }
+
+
+    }
+}
